418a64951cebf15eeb09b3a392336dd6b1ac3c31
eszheng <165437713+eszheng@users.noreply.github.com>
2024-05-23 17:39:17 +0000

    Update +page.svelte
    Signed-off-by: eszheng <165437713+eszheng@users.noreply.github.com>

--- src/routes/widgets/+page.svelte
+++ src/routes/widgets/+page.svelte
@@ -8,7 +8,7 @@
 
     for (const line of lines) {
       const items = line.trim().split(',');
-      if (items[0] === 'Hand') continue;
+      if (items.length !== 2 || items[0] === 'Hand') continue;
       const hand = items[0];
       const freq = parseFloat(items[1]);
 
@@ -16,7 +16,7 @@
         for (const a of 'dhcs') {
           wizard[`${hand[0]}${a}${hand[1]}${a}`] = freq;
         }
-      } else {
+      } else if (hand.length === 2) {
         for (const [a, b] of [['d', 'h'], ['d', 'c'], ['d', 's'], ['h', 'c'], ['h', 's'], ['c', 's']]) {
           if (a !== b) wizard[`${hand[0]}${a}${hand[1]}${b}`] = freq;
         }
@@ -34,11 +34,11 @@
 
 <div class="container">
   <div class="box">
-    <h3>HRC Range</h3>
+    <h3>HRC </h3>
     <textarea bind:value={inputText} on:input={processInput}></textarea>
   </div>
   <div class="box">
-    <h3>GTO Wizard Range</h3>
+    <h3>GTOWizard</h3>
     <textarea bind:value={outputText} readonly></textarea>
     <button on:click={copyOutput}>Copy</button>
   </div>
