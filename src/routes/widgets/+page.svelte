893f70be8301a2b32e431d134433030ba034af49
eszheng <165437713+eszheng@users.noreply.github.com>
2024-05-23 18:11:45 +0000

    Update +page.svelte
    Signed-off-by: eszheng <165437713+eszheng@users.noreply.github.com>

--- src/routes/widgets/+page.svelte
+++ src/routes/widgets/+page.svelte
@@ -1,16 +1,4 @@
 <script>
-  import NavBar from '$lib/components/NavBar.svelte';
-</script>
-
-<NavBar />
-
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-
-<script>
   let inputText = '';
   let outputText = '';
 
@@ -20,7 +8,7 @@
 
     for (const line of lines) {
       const items = line.trim().split(',');
-      if (items.length !== 2 || items[0] === 'Hand') continue;
+      if (items[0] === 'Hand') continue;
       const hand = items[0];
       const freq = parseFloat(items[1]);
 
@@ -28,7 +16,7 @@
         for (const a of 'dhcs') {
           wizard[`${hand[0]}${a}${hand[1]}${a}`] = freq;
         }
-      } else if (hand.length === 2) {
+      } else {
         for (const [a, b] of [['d', 'h'], ['d', 'c'], ['d', 's'], ['h', 'c'], ['h', 's'], ['c', 's']]) {
           if (a !== b) wizard[`${hand[0]}${a}${hand[1]}${b}`] = freq;
         }
@@ -46,11 +34,11 @@
 
 <div class="container">
   <div class="box">
-    <h3>HRC Range</h3>
+    <h3>Input:</h3>
     <textarea bind:value={inputText} on:input={processInput}></textarea>
   </div>
   <div class="box">
-    <h3>GTO Wizard Range</h3>
+    <h3>Output:</h3>
     <textarea bind:value={outputText} readonly></textarea>
     <button on:click={copyOutput}>Copy</button>
   </div>
